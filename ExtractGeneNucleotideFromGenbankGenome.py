3a39c22e5c7cfa871029ce8548f77c30478d03aa
Sanket Desai <desai.sanket12@gmail.com>
2020-03-29 19:34:12 +0530

    For gene, CDS and protein (translation)

--- ExtractGeneNucleotideFromGenbankGenome.py
+++ ExtractGeneNucleotideFromGenbankGenome.py
@@ -1,7 +1,9 @@
 import sys
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
-from Bio import Seq
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_protein
+
 #feature type could be gene, CDS, protein, UTR and so on - please refer to GenBank nomenclature
 '''
 def get_location_start_end_as_list(loc):
@@ -19,14 +21,24 @@ def main():
             print(srec.id)
             for f in srec.features:
                 sid=""
+                gname=""
+                proteinid=""
                 if f.type == sys.argv[2]:
-                    print(f.location.end)
                     for q1 in f.qualifiers:
                         if q1 == 'gene':
                             sid=srec.id+"__"+f.type+"__"+str(f.location.start)+"__"+str(f.location.end)+"__"+f.qualifiers[q1][0]
-                featrecord=SeqRecord(f.extract(srec.seq), id=sid, name="", description="")
-                fo=outdir+sid+".fa"
-                SeqIO.write(featrecord, fo, "fasta")
+                            gname=f.qualifiers[q1][0]
+                            featrecord=SeqRecord(f.extract(srec.seq), id=sid, name="", description="")
+                            fo=outdir+sid+".fa"
+                            SeqIO.write(featrecord, fo, "fasta")
+                        if q1 == 'protein_id':
+                            proteinid=f.qualifiers[q1][0]
+                        if f.type == "CDS" and q1 == 'translation':
+                            proid=srec.id+"__Protein__"+proteinid+"__"+gname
+                            sqq=Seq(f.qualifiers[q1][0], generic_protein,)
+                            featrecord=SeqRecord(sqq, id=proid, name="", description="")
+                            fo=outdir+proid+".fa"
+                            SeqIO.write(featrecord, fo, "fasta")
                 #print(f.extract(srec.seq))
             #print(srec.features[0])
 
